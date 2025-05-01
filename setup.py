b8493b3b7d4bfbdd6e51e8074ecc3a85f5d7eccd
tanmay <you@example.com>
2025-05-01 23:56:41 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Tanmay garg',
+    author_email= 'tanmaygarg833@gamil.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
